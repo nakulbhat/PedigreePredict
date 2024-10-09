e080529f7dca38cb9a83d06f44ad68ec34502bb9
Nakul Bhat <nakulbhat034@gmail.com>
2024-10-09 12:52:28 +0530

    structs defined

---
+++ pedigreeChartLinkedList.c
@@ -0,0 +1,23 @@
+
+typedef enum gender {MALE, FEMALE} gender;
+
+typedef struct Person
+{
+    gender gen;
+    int id;
+    struct Relation* relationOfOrigin;
+    int characteristics[10];
+    struct Person* nextSibling;
+    struct Relation* firstRelation;
+}Person;
+
+typedef struct Relation
+{
+    struct Person* person1;
+    struct Person* person2;
+    struct Person* firstChild;
+    int characteristics[20];
+    struct Relation* p1nextRelation;
+    struct Relation* p2nextRelation;
+};
+
