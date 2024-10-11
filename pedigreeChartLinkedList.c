e5b8baaefdca4fec7e4f858a039fdaa801ac49bf
Nakul Bhat <nakulbhat034@gmail.com>
2024-10-11 12:36:51 +0530

    working commit: addRelation

--- pedigreeChartLinkedList.c
+++ pedigreeChartLinkedList.c
@@ -1,10 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_NAME_LENGTH 25
 
 typedef enum gender {MALE, FEMALE} gender;
 
 typedef struct Person
 {
     gender gender;
-    char name[25];
+    char name[MAX_NAME_LENGTH];
     int id;
     struct Relation* relationOfOrigin;
     int characteristics[10];
@@ -15,12 +20,12 @@ typedef struct Person
 
 typedef struct Relation
 {
-    struct Person* person1;
-    struct Person* person2;
+    struct Person* male;
+    struct Person* female;
     struct Person* firstChild;
     int characteristics[20];
-    struct Relation* p1nextRelation;
-    struct Relation* p2nextRelation;
+    struct Relation* maleNextRelation;
+    struct Relation* femaleNextRelation;
 }Relation;
 
 int generateID(){
@@ -32,11 +37,48 @@ int generateID(){
 Person* createPerson(Relation* relOfOrg, gender gender, char name[] ,int characteristics[]){
     Person* newPerson = (Person*) malloc(sizeof(Person));
     newPerson->id = generateID();
+    strcpy(newPerson->name, name);
+    newPerson->gender = gender;
+    memcpy(newPerson->characteristics, characteristics, sizeof(newPerson->characteristics));
+    return &newPerson;
 }
 
-void addSibling(Relation* relOfOrg, gender gender, char name[] ,int characteristics[]){
+void addSibling(Relation* relOfOrg, gender gender, char name[] ,int characteristics[]){~
+    Person* newPerson = createPerson(relOfOrg, gender, name, characteristics);
 
+    Person* kid = relOfOrg->firstChild;
+
+    if (kid == NULL) relOfOrg->firstChild = newPerson; // if no kids yet, then add as first kid
+    //iterate until a kid has no nextSibling
+    else {
+        while (kid->nextSibling)
+            kid = kid->nextSibling;
+        
+        kid->nextSibling = newPerson;
+    }
 }
+
+Relation* createRelation(Person* male, Person* female){
+    Relation* newRelation = (Relation*) malloc(sizeof(Relation));
+    newRelation->male = male;
+    newRelation->female = female;
+    return &newRelation;
+}
+
+void addRelation(Person* male, Person* female){
+    Relation* newRelation = createRelation(male, female);
+    Relation* relationMale = male->firstRelation;
+    Relation* relationFemale = female->firstRelation;
+
+    //find latest relations of primary and secondary
+
+    while (relationMale && relationMale->maleNextRelation)
+        relationMale = relationMale->maleNextRelation;
+
+    while (relationFemale && relationFemale->femaleNextRelation);
+    
+}
+
 
 int main(){
     return 0;
